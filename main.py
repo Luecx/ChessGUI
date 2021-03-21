172f374bfab1637dfb37c4dea4eb04d0577428e3
Finn Eggers <web@finneggers.de>
2021-03-21 18:07:20 +0100

    added a few things

--- main.py
+++ main.py
@@ -3,12 +3,25 @@ import sys
 import os
 
 
-from PySide2.QtWidgets import QApplication, QMainWindow
-from PySide2.QtCore import QPropertyAnimation, QFile
-from PySide2.QtUiTools import QUiLoader
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtCore import QPropertyAnimation, Qt
+from PyQt5 import uic
 
 import res
 
+class TitleFrame(QWidget):
+    def mousePressEvent(self, event):
+
+        if event.buttons() == Qt.RightButton:
+            self.dragPos = event.globalPos()
+            event.accept()
+
+    def mouseMoveEvent(self, event):
+
+        if event.buttons() == Qt.RightButton:
+            self.move(self.pos() + event.globalPos() - self.dragPos)
+            self.dragPos = event.globalPos()
+            event.accept()
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -16,19 +29,34 @@ class MainWindow(QMainWindow):
         self.load_ui()
 
     def load_ui(self):
-        loader = QUiLoader()
         path = os.path.join(os.path.dirname(__file__), "form.ui")
-        ui_file = QFile(path)
-        ui_file.open(QFile.ReadOnly)
-        self.window = loader.load(ui_file, self)
-#        window.setWindowFlag(Qt.FramelessWindowHint)
-        self.window.menuopen_button.clicked.connect(lambda x: self.menu_animation(x))
-        self.window.show()
-        ui_file.close()
+        uic.loadUi(path, self)
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.menuopen_button.clicked.connect(lambda x: self.menu_animation(x))
+        self.minimise_button.clicked.connect(lambda x:self.menu_buttons('minimise'))
+        self.maximise_button.clicked.connect(lambda x:self.menu_buttons('maximise'))
+        self.close_button.clicked.connect(lambda x:self.menu_buttons('close'))
+
+        self.title_frame.mouseMoveEvent = lambda x: self.window_move_event(x)
+        self.title_frame.setMouseTracking(True)
+#        self.title_frame.setMouseTracking(True)
+#        self.mouse_tracker = MouseTracker(self.window.title_frame)
+#        self.mouse_tracker.positionChanged.connect(self.window_move_event)
+
+    def menu_buttons(self, event):
+        if event == 'maximise':
+            if self.isMaximized():
+                self.showNormal()
+            else:
+                self.showMaximized()
+        if event == 'close':
+            self.close()
+        if event == 'minimise':
+            self.showMinimized()
 
     def menu_animation(self, mode):
-        self.open_menu_anim = QPropertyAnimation(self.window.menu_frame, b"minimumWidth")
-        self.open_menu_anim.setDuration(300)
+        self.open_menu_anim = QPropertyAnimation(self.menu_frame, b"minimumWidth")
+        self.open_menu_anim.setDuration(200)
         if mode:
             self.open_menu_anim.setEndValue(230)
         else:
@@ -36,8 +64,23 @@ class MainWindow(QMainWindow):
 
         self.open_menu_anim.start()
 
+    def window_move_event(self, event):
+        if not hasattr(self, 'old_position'):
+            self.old_position = event.pos()
+            print(self.old_position)
+            return
+        print(event)
+#        print(event.type)
+#        print(event.pos())
+#        self.window.move(self.pos() + event)
+#        self.move(self.pos() + event)
+#        event.accept()
+
+
+
 
 if __name__ == "__main__":
     app = QApplication([])
     widget = MainWindow()
+    widget.show()
     sys.exit(app.exec_())
